14d5f71f12709edfb6d6fee268c8aa43f4a32341
DanielJBailey <Dbailey0210@gmail.com>
2018-11-11 20:23:57 -0700

    merge conflicts

--- casino.rb
+++ casino.rb
@@ -1,11 +1,7 @@
 require_relative './player'
 require_relative './Games/roulette'
-<<<<<<< Updated upstream
 require_relative './Games/slots.rb'
 require_relative './Games/highnlow.rb'
-=======
-require_relative './Games/rockpaperscissors'
->>>>>>> Stashed changes
 require 'sounder'
 
 Sounder::System.set_volume 50 # 0-100
